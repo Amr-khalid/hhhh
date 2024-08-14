da328e78d2608b888adbcf0dcfd85336f3d62466
Amr Khalid <kamr80001@gmail.com>
2024-08-14 14:53:24 +0300

    Add files via upload

---
+++ hh.js
@@ -0,0 +1,8 @@
+let x=document.getElementById('a2')
+x.addEventListener('click',function name(params) {document.images[0].src='a2.jpg'
+    
+})
+let w=document.querySelector('.n2')
+w.addEventListener('click',function name(params) {document.images[0].src='n2.jpg'
+    
+})
